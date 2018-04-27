ee41cfc22fdd583defdaeb9ba3fa11305f068811
Ezequiel Lowi <skulas@gmail.com>
2018-04-27 11:04:59 +0300

    Edit Hero name Formatting Pipe Begin list of Heroes

--- Angular/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Angular/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from './../hero';
+import {NgModel} from '@angular/forms';
 
 @Component({
+  // the component's CSS element selector
   selector: 'app-heroes',
+  // the location of the component's template file
   templateUrl: './heroes.component.html',
+  // the location of the component's private CSS styles
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id : 1,
+    name : 'Tito Gomez'
+  };
 
   constructor() { }
 
---
+++ Angular/tour-of-heroes/src/app/moc-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1551, name: 'Raul'},
+    { id: 243, name: 'Yossi'},
+    { id: 55428, name: 'Sughando'},
+    { id: 11, name: 'narepuS'},
+    { id: 888, name: 'Negro'},
+    { id: 717, name: 'Blanko'},
+    { id: 777, name: 'James'},
+    { id: 666, name: 'jLuci'},
+    { id: 9099, name: 'La 99'}
+];
