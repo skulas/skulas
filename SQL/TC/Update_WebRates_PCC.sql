0a71d6e8d37bf281bfefcc1bd6bf92676ed62c81
Ezequiel <skulas@gmail.com>
2018-05-31 07:47:03 +0000

    Remove irrelevant folder

--- SQL/TC/Update_WebRates_PCC.sql
+++
@@ -1,6 +0,0 @@
-DECLARE @PCC VARCHAR(10) = 'TRIPCHAMP'
-DECLARE	@webRates int = 4
-
-UPDATE [dbo].[SupplierConfigs]
-SET [TestAccountId]=@PCC,[LiveAccountId]=@PCC
-WHERE [Id] = @webRates
--- SQL/TC/suppliers_Turn_Suppliers_OnOff___Used_in_Bird_Prod.sql
+++
@@ -1,3 +0,0 @@
-UPDATE [trums].[dbo].[SupplierConfigs]
-SET IsEnabled=0 /*1*/
-WHERE Code in ('V3.1','V3.2','V3.3','V3.4','V3.5','V3.6','V3.8')
